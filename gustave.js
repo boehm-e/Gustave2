ede7300230d8fd3bc3b4a631a26f264b122e25f4
= <=>
2016-10-08 02:07:25 +0000

    working

--- gustave.js
+++ gustave.js
@@ -1,8 +1,16 @@
 var gustave = require('./system/intent');
+var kws = require('./system/kws')
+
+
 gustave.init()
     .then((success) => {
+
+	// Start kws
+	kws.start(gustave);
+
 	console.log(success)
 	gustave.ask('actu de futura-sciences');
+	gustave.system.tts("coucou")
 	//	console.log(gustave.system.security.checkFace(gustave.system));
 	// .then(() => {
 	// 	gustave.ask("quelle heure est il");
--- gustave_modules/news/index.js
+++ gustave_modules/news/index.js
@@ -1,14 +1,14 @@
 var news = require('./lib/news.js')
 exports.start = (system, string, label) => {
-	return new Promise((resolve, reject) => {
-		news.get(string, label).then((data) => {
-			system.tts(data);
-			return resolve(data);
-		})
-		.catch((err) => {
-			return reject(err);
-		})
+    return new Promise((resolve, reject) => {
+	news.get(string, label).then((data) => {
+//	    system.tts(data);
+	    return resolve(data);
 	})
+	    .catch((err) => {
+		return reject(err);
+	    })
+		})
 }
 
 // exports.render = () {
